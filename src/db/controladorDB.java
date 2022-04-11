44f9244680851aa9f2186e4bfeb1a8e26a8da840
JonAnderUni <jmiguel011@ikasle.ehu.eus>
2022-04-11 17:27:54 +0200

    Controlador mysql (sin terminar)

---
+++ src/db/controladorDB.java
@@ -0,0 +1,11 @@
+package db;
+
+public class controladorDB {
+	//Connection conn = null;
+	private final String usuario = "tap";
+	private final String pass = "tap";
+	private final String url = "jdbc:mysql://188.166.16.194:3306/";
+	private final String dbName = "";
+	private final String driver = "com.mysql.jdbc.Driver";
+	
+}
