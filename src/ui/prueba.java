3fceca2838eebc025bdecd6cb0e8e3471b3ef121
JonAnderUni <jmiguel011@ikasle.ehu.eus>
2022-04-15 10:51:58 +0200

    Una prueba de la parte visual

--- src/ui/prueba.java
+++ src/ui/prueba.java
@@ -40,8 +40,14 @@ public class prueba extends JFrame {
 	 * Create the frame.
 	 */
 	public prueba() {
-		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
